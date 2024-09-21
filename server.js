e7a3dae4b88c0cfe9eab831d2e542c9912f9cdef
robinsilverio <robinsilverio17@gmail.com>
2024-09-21 14:03:29 +0200

    Handling invalid urls.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import e from "express";
 import { startDatabaseConnection } from "./dbclient.js";
-import router from "./server-routes.js";
+import router, { sendResponse } from "./server-routes.js";
+import { statusCodes } from "./src/enums/status-codes.js";
 
 const PORT = process.env.PORT || 3000;
 const app = e();
@@ -8,6 +9,10 @@ const app = e();
 startDatabaseConnection();
 
 app.use('/', router);
+
+app.use((req, res) => {
+    sendResponse(res, statusCodes.NOT_FOUND, { error: "Route not found" });
+});
 
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
