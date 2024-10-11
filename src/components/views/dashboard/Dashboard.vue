b2d71a67ed868567cf6b61a4c035359ab735c655
robinsilverio <robinsilverio17@gmail.com>
2024-10-11 14:55:10 +0200

    Dashboard styling.

--- src/components/views/dashboard/Dashboard.vue
+++ src/components/views/dashboard/Dashboard.vue
@@ -2,6 +2,7 @@
     <DashboardHeaderComponent></DashboardHeaderComponent>
     <div class="dashboard-container">
         <div class="buttons" v-for="button of this.dashboardButtons" :key="button.id">
+            <img :src="button.iconSrc" alt="" height="150" width="150">
             {{ button.value }}
         </div>
     </div>
@@ -18,10 +19,10 @@ export default {
     data() {
         return {
             dashboardButtons: [
-                { id: 1, name: 'manage-candidates', value: 'Manage candidates' },
-                { id: 2, name: 'manage-election-types', value: 'Manage election types' },
-                { id: 3, name: 'manage-ballots', value: 'Manage ballots' },
-                { id: 4, name: 'view-results', value: 'View results per election' }
+                { id: 1, name: 'manage-candidates', value: 'Manage candidates', iconSrc: require('../../../assets/icons/Transparent_Manage_candidates.png') },
+                { id: 2, name: 'manage-election-types', value: 'Manage election types', iconSrc: require('../../../assets/icons/Transparent_Manage_election_types.png') },
+                { id: 3, name: 'manage-ballots', value: 'Manage ballots', iconSrc: require('../../../assets/icons/Transparent_Manage_ballots.png') },
+                { id: 4, name: 'view-results', value: 'View results per election', iconSrc: require('../../../assets/icons/Transparent_View_results_per_election.png') }
             ]
         }
     }
@@ -37,4 +38,14 @@ export default {
         display: flex;
         flex-wrap: wrap;
     }
+    .dashboard-container {
+        justify-content: space-around;
+        padding: 20px 0px;
+    }
+    .buttons {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+    }
+    .buttons img { margin: auto; }
 </style>
